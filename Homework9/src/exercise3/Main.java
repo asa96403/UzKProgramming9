a794f41beee3ab25d4acf84542b241d8dee2400e
aabert <aabert@smail.uni-koeln.de>
2025-06-21 20:05:44 +0200

    added movies

--- Homework9/src/exercise3/Main.java
+++ Homework9/src/exercise3/Main.java
@@ -9,7 +9,16 @@ import java.util.stream.Stream;
 public class Main {
 	public static void main(String[] args) {
 		List<Movie> movies = new ArrayList<Movie>();
+		//Bewertungen meist von amazon oder Prime Video
 		movies.add(new Movie("Harry Potter und der Stein der Weisen", 9.6, 2001));
+		movies.add(new Movie("Harry Potter und die Kammer des Schreckens", 7.4, 2002));
+		movies.add(new Movie("Harry Potter und der Gefangene von Askaban", 7.9, 2004));
+		movies.add(new Movie("Harry Potter und der Feuerkelch", 9.6, 2004));
+		movies.add(new Movie("Harry Potter und der Orden des Phönix", 9.4, 2007));
+		movies.add(new Movie("Harry Potter und der Halbblutprinz", 9.4, 2009));
+		movies.add(new Movie("Harry Potter und die Heiligtümer des Todes", 9.7, 2010));
+		movies.add(new Movie("Sortierungsbeispiel", 9.9, 2010));
+		movies.add(new Movie("Harry Potter und der Halbblutprinz", 9.6, 2011));
 		Stream<Movie> movieStream =movies.stream();
 		Predicate<Movie> rating8 = t -> t.rating()>=8.5; 
 		Comparator<Movie> year = (o1, o2) -> {
